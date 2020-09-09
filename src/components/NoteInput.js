7559277d281ab8f7a14acf9937bf49bb1a221c0a
Bradslavens <bradslavens@gmail.com>
2020-09-09 16:39:18 -0700

    selecting notes works

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react'
 
 class NoteInput extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            notes: [],
+        }
+
+        this.addNotes = this.addNotes.bind(this)
+
+    }
+
+    addNotes(event){
+        console.log("add note clicked ");
+        this.setState(
+            {
+                notes: [...this.state.notes, <div>{event.target.value}</div>],
+            }
+        );
+    }
+
+
     render(){
         return(
             <div>
                 <form>
-                        <select  >
-                            <option value="1">A</option>
-                            <option value="1">A&#9839;/B&#9837;</option>
-                            <option value="2">B</option>
-                            <option value="3">C</option>
-                            <option value="1">C&#9839;/D&#9837;</option>
-                            <option value="1">D</option>
-                            <option value="1">D&#9839;/E&#9837;</option>
-                            <option value="2">E</option>
-                            <option value="3">F</option>
-                            <option value="1">F&#9839;/G&#9837;</option>
-                            <option value="3">G</option>
-                            <option value="1">G&#9839;/A&#9837;</option>
+                        <select  onChange={this.addNotes} >
+                            <option key="1" value="A">A</option>
+                            <option key="2" value="A#Bb">A&#9839;/B&#9837;</option>
+                            <option key="3" value="B">B</option>
+                            <option value="C">C</option>
+                            <option value="C#Db">C&#9839;/D&#9837;</option>
+                            <option value="D">D</option>
+                            <option value="D#Eb">D&#9839;/E&#9837;</option>
+                            <option value="E">E</option>
+                            <option value="F">F</option>
+                            <option value="F#Gb">F&#9839;/G&#9837;</option>
+                            <option value="G">G</option>
+                            <option value="G#Ab">G&#9839;/A&#9837;</option>
                         </select>
                 </form>
-                <button>+ add a note</button>
+                {/* <button onClick={this.addNotes}>+ add a note</button> */}
+                <div>{this.state.notes}</div>
             </div>
         )
     }
