4d87ae3b6f2b32a9a152da7ff0188951e52a956f
Bradslavens <bradslavens@gmail.com>
2020-09-24 12:40:44 -0700

    partial styled results

--- src/components/Results.js
+++ src/components/Results.js
@@ -13,17 +13,17 @@ class Results extends Component {
                 let scale = this.props.scaleMatches[key][type];
                 
             a.push(
-                <p>
-                    <h1>{key}</h1>
-                    <h3>{type}</h3>                    
-                    <h3>{scale}</h3>
+                <p class="bg-gray-400 p-3 m-3">
+                    <h1>Key: {key}</h1>
+                    <h3>Type: {type}</h3>                    
+                    <h3>Scale: {scale}</h3>
                 </p>
                 );
 
             }
         }
         return(
-            <div>
+            <div class="flex flex-wrap">
                 {a}
             </div>
         )
