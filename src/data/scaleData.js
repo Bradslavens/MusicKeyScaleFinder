bd89a7db21aed8a2ed82e5d893129431afa85214
Bradslavens <bradslavens@gmail.com>
2020-09-24 17:11:23 -0700

    added scales from garage band

--- src/data/scaleData.js
+++ src/data/scaleData.js
@@ -1,12 +1,27 @@
 let majorScale = [2,2,1,2,2,2,1];
-// let majorScale = [1,1,0,1,1,1,0];
 
 let minorScale = [2,1,2,2,1,2,2,];
 
-// let harmonicMinor = [2,1,2,2,1,3,1,]
+let harmonicMinor = [2,1,2,2,1,3,1,];
+
+let dorian = [2,1,2,2,2,1,2,];
+
+let minorPentatonic = [3,2,2,3,2];
+
+let minorBlues = [3,2,1,1,3,2,];
+
+let japanese = [1,4,2,3,2];
+
+let majorPentatonic = [2,2,3,2,3];
+
+let majorBlues = [2,1,1,3,2,1,3];
+
+let mixolydian = [2,2,1,2,2,1,2];
+
+let klezmer = [2,1,3,1,2,1,2,];
 
 
-let extension = ["major", "minor", "harmonicMinor","dorian"];
+let extension = ["major", "minor", "harmonicMinor","dorian","minorPentatonic","minorBlues","japanese","majorPentatonic","majorBlues",];
 
 let key = ["A","AsBb","B","C","CsDb","D","DsEb","E","F","FsGf","G","GsAb"];
 let resetkey = ["A","AsBb","B","C","CsDb","D","DsEb","E","F","FsGf","G","GsAb"];
@@ -23,16 +38,52 @@ for(let i = 0; i<key.length; i++){
 	for(let j = 0; j<extension.length; j++){
     
         let b = a[extension[j]] = [];
-        
+
         if(extension[j].localeCompare("major") === 0){
 
             generateScale(majorScale, b);
 
-        }  else if (extension[j].localeCompare("minor") === 0){
+        }   else if (extension[j].localeCompare("minor") === 0){
 
             generateScale(minorScale, b);
 
-        }			
+        }	else if (extension[j].localeCompare("harmonicMinor") === 0){
+
+            generateScale(harmonicMinor, b);
+
+        }	else if (extension[j].localeCompare("dorian") === 0){
+
+            generateScale(dorian, b);
+
+        }	else if (extension[j].localeCompare("minorPentatonic") === 0){
+
+            generateScale(minorPentatonic, b);
+
+        }	else if (extension[j].localeCompare("minorBlues") === 0){
+
+            generateScale(minorBlues, b);
+
+        }	else if (extension[j].localeCompare("japanese") === 0){
+
+            generateScale(japanese, b);
+
+        }	else if (extension[j].localeCompare("majorPentatonic") === 0){
+
+            generateScale(majorPentatonic, b);
+
+        }	else if (extension[j].localeCompare("majorBlues") === 0){
+
+            generateScale(majorBlues, b);
+
+        }	else if (extension[j].localeCompare("mixolydian") === 0){
+
+            generateScale(mixolydian, b);
+
+        }	else if (extension[j].localeCompare("klezmer") === 0){
+
+            generateScale(klezmer, b);
+
+        }	
     }
     for(let y=0;y<key.length; y++){
         key[y] = resetkey[y];
